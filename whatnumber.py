f8453320ac471b9852ab562dc7afbfe86812e1c4
Simone <37227082+simonefullstack@users.noreply.github.com>
2025-10-18 20:20:10 -0400

    Add files via upload
    Guessing game

---
+++ whatnumber.py
@@ -0,0 +1,21 @@
+#This is a game to guess the numbers between 0 and 10. The player has 3 tries to guess the correct number.
+
+import random
+
+#generate random number to be guessed
+random_num = random.randint(0, 10)
+
+#number of guesses each person gets
+num_guess = 3
+
+#for loop to give the three chances to guess and evealuate if number has been guessed.
+for i in range(num_guess):
+    guess = int(input("What is your guess?"))
+
+    # if random number is guessed, break loop and let person know
+    if random_num == guess:
+        print("Congratulations!! You guessed the number.")
+        break
+
+#gives random number
+print("The random number is ", + random_num)
